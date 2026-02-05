12240d4cbb85e974c284f5a294b309230241b232
musakhanbaloch03-sad <musakhanbaloch03@gmail.com>
2026-02-05 17:54:14 +0500

    Create group-promote.js

---
+++ plugins/group-promote.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.isGroup) return m.reply('❌ *This command is only for groups!*');
+  
+  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+  
+  if (!user) return m.reply(`❌ *Tag someone to promote!*\n\n*Example:* ${usedPrefix + command} @user`);
+  
+  let botAdmin = await conn.groupMetadata(m.chat).then(data => {
+    return data.participants.find(v => v.id === conn.user.jid)?.admin;
+  });
+  
+  if (!botAdmin) return m.reply('❌ *I need to be admin to promote members!*');
+  
+  await conn.groupParticipantsUpdate(m.chat, [user], 'promote');
+  
+  await m.reply(`✅ *Successfully promoted @${user.split('@')[0]} to admin!*\n\n_© YOUSAF-BALOCH-MD | Muhammad Yousaf Baloch_`, null, { mentions: [user] });
+};
+
+handler.help = ['promote'];
+handler.tags = ['group'];
+handler.command = /^(promote|admin)$/i;
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
